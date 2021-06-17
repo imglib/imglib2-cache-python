4793381270aa6e6b52542ac0cadefb6696fac1aa
Philipp Hanslovsky <philipp.hanslovsky@gmail.com>
2021-06-16 22:29:31 -0400

    Revert "Temporarily fail code coverage gate"
    This reverts commit 402de83725d5aae65dcad2eacf09d04ec840764e.

--- .github/workflows/code-coverage.sh
+++ .github/workflows/code-coverage.sh
@@ -7,7 +7,7 @@ INSTRUCTION_MISSED=$(cut -d, -f4 target/site/jacoco/jacoco.csv | tail -n+2 | pas
 INSTRUCTION_COVERED=$(cut -d, -f5 target/site/jacoco/jacoco.csv | tail -n+2 | paste -sd+ | bc)
 INSTRUCTION_TOTAL=$((INSTRUCTION_MISSED + INSTRUCTION_COVERED))
 COVERAGE=$(echo "$INSTRUCTION_COVERED * 1.0 / $INSTRUCTION_TOTAL" | bc -l)
-THRESHOLD=0.99
+THRESHOLD=0.8
 FAILED=$(echo "$COVERAGE<$THRESHOLD" | bc -l)
 echo Code coverage is $COVERAGE
 if [ "$FAILED" -eq "1" ]; then
